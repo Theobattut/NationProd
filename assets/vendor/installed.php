c22e10638ab004276a572e9feeefe237de02e59b
TheoBattut <theobattut@hotmail.fr>
2026-04-09 20:06:03 +0200

    assets files vendor

---
+++ assets/vendor/installed.php
@@ -0,0 +1,92 @@
+<?php return array (
+  '@hotwired/stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '7.3.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'bootstrap' => 
+  array (
+    'version' => '5.3.3',
+    'dependencies' => 
+    array (
+      0 => '@popperjs/core',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@popperjs/core' => 
+  array (
+    'version' => '2.11.8',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'bootstrap/dist/css/bootstrap.min.css' => 
+  array (
+    'version' => '5.3.3',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@fortawesome/fontawesome-free/css/all.css' => 
+  array (
+    'version' => '6.7.1',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+      0 => '/webfonts/fa-brands-400.woff2',
+      1 => '/webfonts/fa-brands-400.ttf',
+      2 => '/webfonts/fa-regular-400.woff2',
+      3 => '/webfonts/fa-regular-400.ttf',
+      4 => '/webfonts/fa-solid-900.woff2',
+      5 => '/webfonts/fa-solid-900.ttf',
+      6 => '/webfonts/fa-v4compatibility.woff2',
+      7 => '/webfonts/fa-v4compatibility.ttf',
+    ),
+  ),
+  'flip-toolkit' => 
+  array (
+    'version' => '7.2.6',
+    'dependencies' => 
+    array (
+      0 => 'rematrix',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'rematrix' => 
+  array (
+    'version' => '0.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
